faf438fa5dc3e36f575c9d5b9c93bec2dce0990f
kbs0621 <jjjustin7531@gmail.com>
2025-08-12 16:10:01 +0900

    Arguments setting update

--- src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Component;
 public class MyPropsRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("VM Arguments = " + args.containsOption("foo"));
+        System.out.println("Program Arguments = " + args.containsOption("bar"));
+
+        for (String argName : args.getOptionNames()){
+            System.out.println("아규먼트 이름 = " + argName);
+        }
 
     }
 }
