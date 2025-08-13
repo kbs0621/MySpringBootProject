c440e812bbeb8a5225f6bb3276c03094c2b512f2
kbs0621 <jjjustin7531@gmail.com>
2025-08-13 10:55:02 +0900

    ProdConfig update

---
+++ src/main/java/com/rookies4/myspringboot/config/ProdConfig.java
@@ -0,0 +1,20 @@
+package com.rookies4.myspringboot.config;
+
+
+import com.rookies4.myspringboot.config.vo.CustomVO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ProdConfig {
+    @Bean
+    public CustomVO customVO(){
+        return CustomVO.builder() //CustomVOBuilder
+                .mode("운영모드")
+                .rate(1.5)
+                .build(); //CustomerVO
+
+    }
+
+}
