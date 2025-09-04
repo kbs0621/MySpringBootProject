607a4917bebd0696a04417dc6508bfe1e7611d1f
kbs0621 <jjjustin7531@gmail.com>
2025-09-04 16:22:22 +0900

    update

--- src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
+++ src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
@@ -19,6 +19,7 @@ public class UserInfoController {
     private UserInfoRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -30,26 +31,27 @@ public class UserInfoController {
     }
 
     @PostMapping("/new")
-    public String addNewUser(@RequestBody UserInfo userInfo) {
+    public String addNewUser(@RequestBody UserInfo userInfo){
         userInfo.setPassword(
                 passwordEncoder.encode(userInfo.getPassword()));
         UserInfo savedUserInfo = repository.save(userInfo);
         return savedUserInfo.getName() + " user added!!";
     }
 
-
     @PostMapping("/login")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        //인증 과정 처리
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         authRequest.getEmail(),
                         authRequest.getPassword()
                 ));
+        //인증 성공여부 체크
         if (authentication.isAuthenticated()) {
+            //인증 성공하면 토큰 생성
             return jwtService.generateToken(authRequest.getEmail());
         } else {
             throw new UsernameNotFoundException("invalid user request !");
         }
     }
-
 }
